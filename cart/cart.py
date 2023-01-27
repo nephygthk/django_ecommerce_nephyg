0129a8efadd779c85498badd6478e029195332d2
Demian Chidi <demianchidi@gmail.com>
2023-01-27 13:50:45 +0100

    expanded the database and changed the view a little bit to suit mptt, prefetch_related

--- cart/cart.py
+++ cart/cart.py
@@ -1,11 +1,11 @@
+from decimal import Decimal
+
 from django.conf import settings
 
-from decimal import Decimal
 from store.models import Product
 
 
 class Cart:
-
     def __init__(self, request):
 
         # this gets us the session
@@ -22,39 +22,38 @@ class Cart:
         product_id = str(product.id)
 
         if product_id in self.cart:
-            self.cart[product_id]['qty'] = qty
+            self.cart[product_id]["qty"] = qty
         else:
-            self.cart[product_id] = {'price': str(product.price), 'qty':qty}
+            self.cart[product_id] = {"price": str(product.regular_price), "qty": qty}
         self.save()
-    
+
     def __iter__(self):
         # here we get the product from cart, filter the database with the cart product ids to be able to iterate through the list and return the data
         product_ids = self.cart.keys()
-        products = Product.products.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=product_ids)
         # here we copy the cart to have a copy of the basket for iteration
         cart = self.cart.copy()
 
         for product in products:
-            cart[str(product.id)] ['product'] = product
+            cart[str(product.id)]["product"] = product
         for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['qty']
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["qty"]
             yield item
-
 
     def __len__(self):
         # we are getting the quantity of the items in our cart to display on cart count
-        return sum(item['qty'] for item in self.cart.values())
+        return sum(item["qty"] for item in self.cart.values())
 
     # def get_total_price(self):
     #     return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
 
     def get_subtotal_price(self):
-        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+        return sum(Decimal(item["price"]) * item["qty"] for item in self.cart.values())
 
     def get_total_price(self):
 
-        subtotal = sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+        subtotal = sum(Decimal(item["price"]) * item["qty"] for item in self.cart.values())
 
         if subtotal == 0:
             shipping = Decimal(0.00)
@@ -68,7 +67,7 @@ class Cart:
         product_id = str(product)
 
         if product_id in self.cart:
-            self.cart[product_id]['qty'] = qty
+            self.cart[product_id]["qty"] = qty
         self.save()
 
     def remove(self, product):
@@ -84,4 +83,3 @@ class Cart:
 
     def save(self):
         self.session.modified = True
-
